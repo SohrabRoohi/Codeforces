76afb9f856ff63d3e599ad7af58d75fbe156aab2
Sohrab Roohi <sohrabroohi@ufl.edu>
2018-06-25 17:57:41 -0400

    Added Games

---
+++ 268A.cpp
@@ -0,0 +1,62 @@
+/*
+  Sohrab Roohi
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+//If you are looking at my solution this is just a shortcut method and is not always used
+vector<string> split(string input, char c) {
+    stringstream test(input);
+    string segment;
+    vector<string> list;
+    while(getline(test, segment, c))
+    {
+        list.push_back(segment);
+    }
+    return list;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    VI home;
+    VI guest;
+    for(int i = 0; i < n; i++) {
+        int h, g;
+        cin >> h >> g;
+        home.push_back(h);
+        guest.push_back(g);
+    }
+    int number = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) {
+                continue;
+            }
+            if(home[i] == guest[j]) {
+                number++;
+            }
+        }
+    }
+    cout << number;
+    return 0;
+}
