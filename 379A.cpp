be0134cebfbd1ab5607ce00d383652e27ccd34db
Sohrab Roohi <sohrabroohi@ufl.edu>
2018-06-25 19:10:10 -0400

    Added New Year Candles

---
+++ 379A.cpp
@@ -0,0 +1,51 @@
+/*
+  Sohrab Roohi
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+//If you are looking at my solution this is just a shortcut method and is not always used
+vector<string> split(string input, char c) {
+    stringstream test(input);
+    string segment;
+    vector<string> list;
+    while(getline(test, segment, c))
+    {
+        list.push_back(segment);
+    }
+    return list;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    int hours = 0;
+    for(int i = 1; a > 0; i++) {
+        a--;
+        if(i % b == 0) {
+            a++;
+        }
+        hours++;
+    }
+    cout << hours;
+    return 0;
+}
