8542aec96e64c4defededdd477b683d5028128ab
Sohrab Roohi <sohrabroohi@ufl.edu>
2018-06-25 18:38:10 -0400

    Added Is your horseshoe on the other hoof?

---
+++ 228A.cpp
@@ -0,0 +1,47 @@
+/*
+  Sohrab Roohi
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+//If you are looking at my solution this is just a shortcut method and is not always used
+vector<string> split(string input, char c) {
+    stringstream test(input);
+    string segment;
+    vector<string> list;
+    while(getline(test, segment, c))
+    {
+        list.push_back(segment);
+    }
+    return list;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    set<int64> values;
+    for(int i = 0; i < 4; i++) {
+        int64 s;
+        cin >> s;
+        values.insert(s);
+    }
+    cout << 4 - values.size();
+    return 0;
+}
