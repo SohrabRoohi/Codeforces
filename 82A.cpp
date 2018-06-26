1e275f601169890be545d0dbcad38a79b3d4178c
Sohrab Roohi <sohrabroohi@ufl.edu>
2018-06-25 22:28:15 -0400

    Added Double Cola

---
+++ 82A.cpp
@@ -0,0 +1,47 @@
+/*
+  Sohrab Roohi
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+//If you are looking at my solution this is just a shortcut method and is not always used
+vector<string> split(string input, char c) {
+    stringstream test(input);
+    string segment;
+    vector<string> list;
+    while(getline(test, segment, c))
+    {
+        list.push_back(segment);
+    }
+    return list;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int64 n;
+    cin >> n;
+    string names[5] = { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    while(n > 5) {
+        n = n / 2 - 2;
+    }
+    cout << names[n-1];
+    return 0;
+}
